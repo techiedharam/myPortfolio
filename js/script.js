463ab7cf7642d1ac4635079c603a73143822d151
Dharmender <dharmender7077@gmail.com>
2024-02-12 22:09:55 +0530

    Portfolio Responsive

--- js/script.js
+++ js/script.js
@@ -86,35 +86,72 @@ window.addEventListener('scroll', function() {
 });
 
   // Initialize Swiper
+  // var swiper = new Swiper(".card-slider", {
+  //   slidesPerView: 3,
+  //   spaceBetween: 30,
+  //   loop : true ,
+  //   autoplay: {
+  //       delay: 2500,
+  //       disableOnInteraction: false,
+  //     },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //   },
+  //   navigation: {
+  //       nextEl: ".swiper-button-next",
+  //       prevEl: ".swiper-button-prev",
+  //   },
+  //   breakpoints: {
+  //     640: {
+  //       slidesPerView: 2,
+  //       spaceBetween: 20,
+  //     },
+  //     768: {
+  //       slidesPerView: 4,
+  //       spaceBetween: 40,
+  //     },
+  //     1024: {
+  //       slidesPerView: 3,
+  //       spaceBetween: 50,
+  //     },
+  //   },
+  // });
+
+
   var swiper = new Swiper(".card-slider", {
     slidesPerView: 3,
     spaceBetween: 30,
-    loop : true ,
+    loop: true,
     autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
+      delay: 2500,
+      disableOnInteraction: false,
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
-    // breakpoints: {
-    //     640: {
-    //       slidesPerView: 2,
-    //       spaceBetween: 20,
-    //     },
-    //     768: {
-    //       slidesPerView: 4,
-    //       spaceBetween: 40,
-    //     },
-    //     1024: {
-    //       slidesPerView: 5,
-    //       spaceBetween: 50,
-    //     },
-    //   },
+    breakpoints: {
+      477: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
   });
+  
+  
+  
+  
   
