df3ef8108d45ef925a0fa0bb572175a6982e0aa5
Dharmender <dharmender7077@gmail.com>
2024-02-12 22:21:59 +0530

    Portfolio Responsive

--- js/script.js
+++ js/script.js
@@ -120,35 +120,32 @@ window.addEventListener('scroll', function() {
 
 
   var swiper = new Swiper(".card-slider", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    loop: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
     breakpoints: {
-      477: {
-        slidesPerView: 1,
-        spaceBetween: 20,
+      640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
       },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 40,
+      715: {
+          slidesPerView: 2,
+          spaceBetween: 20,
       },
       1024: {
-        slidesPerView: 3,
-        spaceBetween: 50,
+          slidesPerView: 3,
+          spaceBetween: 20,
       },
-    },
+  },
+  pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+  },
+  autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+  },
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
   });
   
   
